7958bcf19cceb7e26af77e5fbfef4ef61a26a127
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-13 23:51:36 +0200

    spacing

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Swrle</div>;
+  return <div className="text-sm font-semibold hidden">Swrle</div>;
 }
 
 export default Username;
