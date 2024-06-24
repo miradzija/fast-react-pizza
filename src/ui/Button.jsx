e757afe3fec297a7fdd27fc134f3d8b3d4b1026c
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-24 23:58:16 +0200

    cart style

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
 
   const styles = {
     primary: base + " md:px-6 md:py-4 py-3 px-4 ",
     small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
+    secondary:
+      "bg-transparent uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed",
   };
 
   if (to)
