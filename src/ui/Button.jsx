adb27fc35daea21658460f028ec37333715a2eef
Milos Zivanovic <zivanovicmilos86@gmail.com>
2024-06-24 23:39:39 +0200

    menu style

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,23 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed ";
+
+  const styles = {
+    primary: base + " md:px-6 md:py-4 py-3 px-4 ",
+    small: base + " py-2 md:px-5 md:py-2.5 px-4 text-xs",
+  };
 
   if (to)
     return (
-      <Link to={to} className={className}>
+      <Link to={to} className={styles[type]}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
